d0962e4699360de9fe2d3532ae437cdbb4900379
AutMaple <838498279@qq.com>
2020-11-07 11:34:12 +0800

    求三个数的最小公倍数

---
+++ min_common_multiple/index.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int common_divisor(int a, int b)
+{
+  int tmp;
+  do
+  {
+    if(a < b){
+      tmp = b;
+      b = a;
+      a = tmp;
+    }
+    tmp = b;
+    b = a % b;
+    a = tmp;
+  }while(b);
+
+  return tmp;
+}
+
+int main()
+{
+  int a; 
+  int b;
+  int c;
+  scanf("%d %d %d", &a, &b, &c);
+  int tmp1 = common_divisor(a, b);
+  int min_multiple1 = a * b / tmp1;
+  int tmp2 = common_divisor(min_multiple1, c);
+  int result = min_multiple1 * c / tmp2;
+  printf("%d, %d, %d 的最小公倍数是: %d\n", a, b, c, result);
+  return 0;
+}
