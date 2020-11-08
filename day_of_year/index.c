7a106da28c135e3a04bc45bb1c8ab35ad5bb967a
AutMaple <maple@DESKTOP-POOVBGT.localdomain>
2020-11-08 21:01:29 +0800

    计算某日是该年的第几天

---
+++ day_of_year/index.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+  int day, month, year, sum;
+  printf("请输入年、月、日，格式为: 年，月，日(2016,07,07)\n");
+  scanf("%d,%d,%d", &year, &month, &day);
+  switch(month){
+    case 1:
+      sum = 0;break;
+    case 2:
+      sum = 31;break;
+    case 3:
+      sum = 59;break;
+    case 4:
+      sum = 90;break;
+    case 5:
+      sum = 120;break;
+    case 6:
+      sum = 151;break;
+    case 7:
+      sum = 181;break;
+    case 8:
+      sum = 212;break;
+    case 9:
+      sum = 243;break;
+    case 10:
+      sum = 273;break;
+    case 11:
+      sum = 304;break;
+    case 12:
+      sum = 334;break;
+  }
+  sum = sum + day;
+  if((year % 4 == 0 && year % 100 == 0) || year % 400 == 0){
+    sum++;
+  }
+  printf("这是这一年的第%d天。\n", sum);
+  return 0;
+}
