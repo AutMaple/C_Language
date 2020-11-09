fcd9c37b0dab3dc5b74f758a187071cf5e0e0673
AutMaple <838498279@qq.com>
2020-11-09 15:24:53 +0800

    打印字母c

---
+++ print_c/index.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("用*打印出字母C\n\n");
+  printf("*******\n");
+  printf("*\n");
+  printf("*\n");
+  printf("*\n");
+  printf("*\n");
+  printf("*\n");
+  printf("*******\n");
+  return 0;
+}
