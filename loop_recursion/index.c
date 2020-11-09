e06a6042f9fa614287b43d61ac13716ab8d109bd
AutMaple <maple@DESKTOP-POOVBGT.localdomain>
2020-11-09 19:50:19 +0800

    模块化代码,便于阅读和维护

--- loop_recursion/index.c
+++ loop_recursion/index.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+float fac_recursion(int n);
+float fac_loop(int n);
+
+
+int main()
+{
+  int n;
+  float result;
+  scanf("%d", &n);
+  result = fac_recursion(n);
+  printf("递归算法求得%d! = %.0f \n", n, result);
+  result = fac_loop(n);
+  printf("循环算法求得%d! = %.0f \n", n, result);
+  return 0; 
+}
+
+
 float fac_recursion(int n){
   if(n == 1){
     return 1.0;
@@ -13,16 +30,4 @@ float fac_loop(int n){
     sum = sum * i;
   }
   return sum;
-}
-
-int main()
-{
-  int n;
-  float result;
-  scanf("%d", &n);
-  result = fac_recursion(n);
-  printf("递归算法求得%d! = %.0f \n", n, result);
-  result = fac_loop(n);
-  printf("循环算法求得%d! = %.0f \n", n, result);
-  return 0; 
 }
