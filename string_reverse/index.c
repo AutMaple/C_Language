9ba4984da372a228473024cb2d5f37f7f82a29b6
AutMaple <maple@DESKTOP-POOVBGT.localdomain>
2020-11-12 17:00:43 +0800

    将各功能模块化

--- string_reverse/index.c
+++ string_reverse/index.c
@@ -3,19 +3,49 @@
 #define max 100
 
 
+int str_len(char* str);
+char* reverse_str(char* str);
+void print_str(char* str);
+
 int main(void)
 {
   char str[max];
+  char* new_str;
   scanf("%[^\n]s", str);
-  int k = 0; 
-  int counter = 0;
-  while(str[k] != '\0'){
-    counter++;
-    k++;
+  new_str = reverse_str(str);
+  print_str(new_str); 
+  return 0;
+}
+
+int str_len(char* str)
+{
+  int len = 0;
+  while(str[len] != '\0')
+  {
+    len++;
   }
-  for(int i = counter - 1; i >= 0; i--){
+  return len;
+}
+
+char* reverse_str(char* str)
+{
+  int length = str_len(str);
+  for(int i = 0; i < length / 2; i++)
+  {
+    char tmp = str[i];
+    str[i] = str[length-i-1];
+    str[length-i-1] = tmp;
+  }
+  return str;
+}
+
+void print_str(char* str)
+{
+  int i = 0;
+  while(str[i] != '\0')
+  {
     printf("%c", str[i]);
+    i++;
   }
   printf("\n");
-  return 0;
 }
